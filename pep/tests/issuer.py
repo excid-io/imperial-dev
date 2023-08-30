1128e39292c50fa8818045f7630c41696353f156
Nikos Fotiou <nikosft@gmail.com>
2023-08-31 01:09:18 +0300

    HMAC support

--- pep/tests/issuer.py
+++ pep/tests/issuer.py
@@ -185,35 +185,4 @@ class Issuer:
         vc.make_signed_token( self.key)
         return vc.serialize()
 
-    def issue_valid_vc_hmac(self, credentialSubjectId):
-        key = jwk.JWK.generate(kty='oct', size=256)
-        jwt_header = {
-            "typ": "jwt",
-            "alg": "HS256",
-        }
-        jwt_claims = {
-            "iss": "http://testscript",
-            "vc": {
-                "@context": [
-                "https://www.w3.org/2018/credentials/v1",
-                "https://excid-io.github.io/imperial/contexts/v1"
-                ],
-                "type": ["VerifiableCredential","RelationshipsCredential"],
-                "credentialSubject": {
-                    "id": credentialSubjectId,
-                    "relationships": [{
-                       "ACME":"authorized"
-                    }]
-                }
-            }
-        }
-        vc = jwt.JWT(header=jwt_header, claims=jwt_claims)
-        vc.make_signed_token(key)
-        return vc.serialize()     
-
-if __name__ == '__main__':
-    key_dict = {'kty': 'EC', 'crv': 'P-256', 'x': 'z30WuxpsPow8KpH0N93vW24nA0HD48_MluqgdEUvtU4', 'y': 'VcKco12BZFPu5HU2LBLotTD9NitdlNxnBLngD-eTapM', 'd': 'UCe_iiyGTQf13KyLPhLgjVCT3gSx4APgNSbS7uyLxN8'}
-    key = jwk.JWK.from_json(json.dumps(key_dict))
-    issuer = Issuer()
-    print (issuer.issue_valid_vc_hmac("did:self:" + key.thumbprint()))
 
